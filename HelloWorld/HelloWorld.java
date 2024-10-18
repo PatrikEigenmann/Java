61ec68a5fb8904839310712b78a012421d67fc18
PatrikEigenmann <p.eigenmann@gmx.net>
2024-10-18 00:49:06 -0700

    This is the first Version to create the new branch.

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,31 @@
+/* -------------------------------------------------------------------------------------------------------
+ * The java class HelloWorld creates the classic "Hello World." program that displays the message “Hello
+ * Java World.” on the console.
+ * -------------------------------------------------------------------------------------------------------
+ * Author:	Patrik Eigenmann
+ * eMail:	p.eigenmann@gmx.net
+ * -------------------------------------------------------------------------------------------------------
+ * Change Log:
+ * Wed 2024-10-17 Class file created.														Version 00.01
+ * -------------------------------------------------------------------------------------------------------*/
+
+/**
+ * The java class HelloWorld creates the classic "Hello World." program that displays the message “Hello
+ * Java World.” on the console.
+ */
+public class HelloWorld {
+
+	/**
+	 * This method is the entry point of the application and starts the Java program. As a simple console
+	 * application, I have integrated a short c program to start the application. Or you can type into
+	 * the terminal java HelloWorld.
+	 * 
+	 * To compile the program, please type javac HelloWorld.java. Thank you.
+	 * 
+	 * @param args are the command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java World.");
+	}
+}
