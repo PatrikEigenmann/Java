3eb11c636ed359fd2e63f48a515e2a5eecd4da4b
Patrik Eigenmann <p.eigenmann@gmx.net>
2024-10-19 02:49:36 -0700

    Changes in the jEnigma.c program. Optimized for Windows & MacOS.

--- jEnigma/jEnigma.c
+++ jEnigma/jEnigma.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /* ***************************************************************************************************
  * jEnigma.c - Starter Programm to run a Java application.
  * Please rename all <name> tags with the actual name of the java class, replace <day> and <date>
  * with the actual day (3 letter word) and date (Format YYYY-MM-DD), make sure there are no typos.
  * Compile and link the c program, and your good to go.
+ *
+ * Compiler information:
+ * gcc jEnigma.c -o jEnigma(.bin)
+ * clang jEnigma.c -o jEnigma(.bin)
  * ---------------------------------------------------------------------------------------------------
  * Author:       Patrik Eigenmann
  * eMail:        p.eigenmann@gmx.net
@@ -16,13 +17,16 @@
  * Tue 2024-10-01 File created.                                                         Version: 00.03
  * Mon 2024-10-14 Optimized for Mac OS.													Version: 00.04
  * ***************************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char **argv) {
-
-    // calling the java virtual machine on command line level but also hide the command line promt.
-    // Windows:             "> NUL 2>&1" to hide the command prompt.
-    // Linux/Unix/MacOS:    "> /dev/null 2>&1" to hide the command promt.
-    // MacOS -> for some reason, /dev/null 2>&1 gives me an error message and does not work.
-    system("java -classpath \"/Users/patrik/development/java/Samael:./\" jEnigma");
+	
+	// Pleas uncomment whichever fits best for you, and change the path to your Samael project or where
+	// you have your java libraries and jar-files. Thank you.
+    // Windows:				-> system("java -classpath \"D:\\Development\\Java\\Samael;.\" jEnigma");
+    // Linux/Unix/MacOS:    -> system("java -classpath \"/home/development/java/Samael:./\" jEnigma");
+    // MacOS:				-> system("java -classpath \"/Users/patrik/development/java/Samael:./\" jEnigma");
+    system("java -classpath \"D:\\Development\\Java\\Samael;.\" jEnigma");
     return 0;
 }
