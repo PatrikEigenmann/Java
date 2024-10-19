b42b4ec4d8c8171188cc3660deff1087ba25e7af
Patrik Eigenmann <p.eigenmann@gmx.net>
2024-10-19 02:30:21 -0700

    Commit the changes for the jHelloWorld.c program. Optimized for Windows & MacOS.

--- jHelloWorld/jHelloWorld.c
+++ jHelloWorld/jHelloWorld.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /* ***************************************************************************************************
  * jHelloWorld.c - Starter Programm to run a Java application.
  * Please rename all <name> tags with the actual name of the java class, replace <day> and <date>
  * with the actual day (3 letter word) and date (Format YYYY-MM-DD), make sure there are no typos.
  * Compile and link the c program, and your good to go.
+ *
+ * Compiler information:
+ * gcc jHelloWorld.c -o jHelloWorld
+ * clang jHelloWorld.c -o jHelloWorld
  * ---------------------------------------------------------------------------------------------------
  * Author:       Patrik Eigenmann
  * eMail:        p.eigenmann@gmx.net
@@ -14,13 +15,15 @@
  * Sun 2024-09-01 System call java corrected. Working -classpath parameter.             Version: 00.01
  * Sun 2024-09-01 Hide the command prompt                                               Version: 00.02
  * Mon 2024-09-09 File created.                                                     	Version: 00.03
+ * Sat 2024-10-19 Optimized for MacOS and Windows.										Version: 00.04
  * ***************************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char **argv) {
 
-    // calling the java virtual machine on command line level but also hide the command line promt.
-    // Windows:             "> NUL 2>&1" to hide the command prompt.
-    // Linux/Unix/MacOS:    "> /dev/null 2>&1" to hide the command promt.
+	// Windows:				-> system("java -classpath \".\" jHelloWorld");
+	// Unix/Linux/MacOS:	-> system("java -classpath \".\" jHelloWorld");
     system("java -classpath \".\" jHelloWorld");
     return 0;
 }
